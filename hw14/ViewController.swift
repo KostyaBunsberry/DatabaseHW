5ba84952a65bede89765f25f0ffef12b9050063d
Kostya Bunsberry <kostya.bunsberry@gmail.com>
2020-08-02 19:12:53 +0700

    Initial Commit

---
+++ hw14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw14
+//
+//  Created by Kostya Bunsberry on 02.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
